691c37cee8c94cec48acdd2dc10176ef59bfb99b
Shuo Chen <shuo.chen@epigenesys.org.uk>
2024-08-09 10:45:39 +0100

    Gem update

--- config/deploy.rb
+++ config/deploy.rb
@@ -120,6 +120,13 @@ namespace :deploy do
   end
 end
 
+namespace :assets do
+  desc "Ensures that dependencies required to compile assets are installed"
+  task install_dependencies: :environment do
+    raise if File.exist?("package.json") && !(system "yarn install --immutable")
+  end
+end
+Rake::Task["assets:precompile"].enhance ["assets:install_dependencies"]
 Rake::Task["deploy:assets:backup_manifest"].clear_actions
 
 ## Restart delayed_job during the deployment process
