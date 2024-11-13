dae62c6099a5409f8e3c019535a482ba3092d8a0
Jack Sanders <personal@jacksanders.uk>
2024-11-13 16:19:34 +0000

    feat(rubocop): removed application config and ApplicationRecord model from exclude

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Model for the pre-created ApplicationRecord table
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,18 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails"
+require_relative 'boot'
+
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
 # require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,6 +20,7 @@ require "action_view/railtie"
 Bundler.require(*Rails.groups)
 
 module Project
+  # Contains global application config
   class Application < Rails::Application
     config.load_defaults 7.0
 
@@ -41,10 +44,10 @@ module Project
       g.system_tests         :rspec
       g.integration_tool     :rspec
       g.test_framework       :rspec,
-                             request_specs:   false,
-                             view_specs:       false,
-                             routing_specs:    false,
-                             helper_specs:     false,
+                             request_specs: false,
+                             view_specs: false,
+                             routing_specs: false,
+                             helper_specs: false,
                              controller_specs: false
     end
   end
