90d851ebc9b3af5047219974490d8d17db548a8e
Ethan Watts <ethan.watts@live.co.uk>
2024-11-18 22:02:12 +0000

    fix/correct-linting-errors

--- spec/features/managing_reviews.rb
+++ spec/features/managing_reviews.rb
@@ -1,36 +1,38 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.feature 'Managing reviews' do
-    specify 'I can add a review' do
-        visit '/'
-        click_on 'have your experience heard'
-        fill_in 'Name', with: 'Test User'
-        fill_in 'Content', with: 'Content for the review'
-        click_on 'Create Review'
-        expect(page).to have_content 'Content for the review' 
-    end
+require "rails_helper"
 
-    specify 'I cannot add a review with no content' do
-        visit '/'
-        click_on 'have your experience heard'
-        fill_in 'Name', with: 'Test User'
-        click_on 'Create Review'
-        expect(page).to have_content 'Content can\'t be blank' 
-    end
+RSpec.feature("Managing reviews") do
+  specify "I can add a review" do
+    visit "/"
+    click_on "have your experience heard"
+    fill_in "Name", with: "Test User"
+    fill_in "Content", with: "Content for the review"
+    click_on "Create Review"
+    expect(page).to(have_content("Content for the review"))
+  end
 
-    specify 'I cannot add a review with no name' do
-        visit '/'
-        click_on 'have your experience heard'
-        fill_in 'Content', with: 'Content for the review'
-        click_on 'Create Review'
-        expect(page).to have_content 'Name can\'t be blank' 
-    end
+  specify "I cannot add a review with no content" do
+    visit "/"
+    click_on "have your experience heard"
+    fill_in "Name", with: "Test User"
+    click_on "Create Review"
+    expect(page).to(have_content("Content can't be blank"))
+  end
 
-    specify 'I cannot add a review with no name or content' do
-        visit '/'
-        click_on 'have your experience heard'
-        click_on 'Create Review'
-        expect(page).to have_content 'Name can\'t be blank' 
-        expect(page).to have_content 'Content can\'t be blank'
-    end
+  specify "I cannot add a review with no name" do
+    visit "/"
+    click_on "have your experience heard"
+    fill_in "Content", with: "Content for the review"
+    click_on "Create Review"
+    expect(page).to(have_content("Name can't be blank"))
+  end
+
+  specify "I cannot add a review with no name or content" do
+    visit "/"
+    click_on "have your experience heard"
+    click_on "Create Review"
+    expect(page).to(have_content("Name can't be blank"))
+    expect(page).to(have_content("Content can't be blank"))
+  end
 end
