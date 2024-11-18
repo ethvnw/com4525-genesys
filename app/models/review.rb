c99420cdedf0bc51a8f2f481f9d011f961ba8958
Ethan Watts <ethan.watts@live.co.uk>
2024-11-18 16:41:03 +0000

    feat(reviews): added submit and view review functionality
    Co-authored-by: EllisBarker <EllisBarker@users.noreply.github.com>

---
+++ app/models/review.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                 :bigint           not null, primary key
+#  content            :text
+#  engagement_counter :integer
+#  is_hidden          :boolean
+#  name               :string
+#  order              :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :name, presence: true
+  before_save :default_values
+
+  def default_values
+    self.is_hidden = false
+    self.engagement_counter = 0
+    self.order = 0
+  end
+end
---
+++ spec/factories/reviews.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                 :bigint           not null, primary key
+#  content            :text
+#  engagement_counter :integer
+#  is_hidden          :boolean
+#  name               :string
+#  order              :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+FactoryBot.define do
+  factory :review do
+    content { "MyText" }
+    order { 1 }
+    is_hidden { false }
+    engagement_counter { 1 }
+  end
+end
