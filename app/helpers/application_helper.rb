da05cd99e54f8428358e59d83a6eb8dba212dd98
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-11-20 18:50:52 +0000

    feat(qol): improved packs structure, added js variables div (!4) Merge branch 'feat/js-helpers' into 'main' with the following commits: * fix(packs): renamed global to application in layout
    * fix(helpers): removed print statement
    
    * feat(packs): renamed global back to application
    
    * fix(application-layout): removed unnecessary comment
    
    * feat(js-variables): added helper class/singleton for JS variables div
    
    * feat(qol): added ability to use multiple style/script packs
    
    also separated the pack definitions for better separation of concerns
    
    * fix(eslint): set browser env variable to true
    
    * feat(qol): added support for multiple packs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,29 @@
 
 # Module containing global helper functions
 module ApplicationHelper
+  ##
+  # Converts a list of script pack names into a list of paths
+  # Will return an empty list if `script_packs` is nil
+  # @param script_packs [Array<String>] an array of script pack names
+  # @return [Array<String>] an array of script pack paths
+  def get_script_paths(script_packs)
+    if script_packs.present?
+      script_packs.map { |script_pack| "scriptpacks/#{script_pack}" }
+    else
+      []
+    end
+  end
+
+  ##
+  # Converts a list of style pack names into a list of paths
+  # Will return an empty list if `style_packs` is nil
+  # @param style_packs [Array<String>] an array of style pack names
+  # @return [Array<String>] an array of style pack paths
+  def get_style_paths(style_packs)
+    if style_packs.present?
+      style_packs.map { |style_pack| "stylepacks/#{style_pack}" }
+    else
+      []
+    end
+  end
 end
---
+++ app/packs/scripts/lib/VariablesDiv.js
@@ -0,0 +1,30 @@
+import toCamelCase from './stringUtils';
+
+/**
+ * A class that can be used to interact with hidden divs containing variables from the backend.
+ * A singleton, `variablesDivSingleton`, is provided to interact with the default one.
+ * @param divId {string} the ID of the div to wrap
+ */
+export class VariablesDiv {
+  constructor(divId = 'js-variables') {
+    const divElem = document.getElementById(divId);
+    this.dataset = { ...divElem.dataset };
+  }
+
+  /**
+   * Safely attempts to retrieve a value from the data-* attributes of the js-variables div
+   * @param key {string} the key to retrieve the value for
+   * @returns {any} the value at the key (if one exists, otherwise null)
+   */
+  get(key) {
+    // All dataset attributes are camel case, so convert first
+    return this.dataset[toCamelCase(key)] || null;
+  }
+}
+
+/**
+ * A singleton used to interact with the default variables div (id: js-variables),
+ * which is present on all pages.
+ * @type {VariablesDiv}
+ */
+export const variablesDivSingleton = new VariablesDiv();
