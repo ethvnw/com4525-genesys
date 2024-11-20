273c3f9806109f5990855d90f561fd8f6fcedceb
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-20 23:40:57 +0000

    feat(cancancan): role based access to dashboards

---
+++ app/controllers/reporter_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ReporterController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_reporter
+
+  def dashboard
+  end
+
+  private
+
+  def authorize_reporter
+    authorize!(:access, :reporter_dashboard)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
+
+  # GET: Admin dashboard route
+  get "admin/dashboard", to: "admin#dashboard", as: :admin_dashboard
+
+  # GET: Reporter dashboard route
+  get "reporter/dashboard", to: "reporter#dashboard", as: :reporter_dashboard
 end
