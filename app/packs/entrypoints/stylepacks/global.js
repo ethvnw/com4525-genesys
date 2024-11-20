0beccf1467a0b093e5609f80f81c3b9088fd0a9c
Jack Sanders <personal@jacksanders.uk>
2024-11-20 17:47:58 +0000

    feat(qol): added ability to use multiple style/script packs
    also separated the pack definitions for better separation of concerns

---
+++ app/packs/entrypoints/stylepacks/global.js
@@ -0,0 +1,5 @@
+import 'bootstrap-icons/font/bootstrap-icons';
+import '../../styles/twitter_bootstrap.scss';
+import '../../styles/layout.scss';
+
+require.context('../../images', true);
--- app/packs/entrypoints/styles.js
+++
@@ -1,5 +0,0 @@
-import 'bootstrap-icons/font/bootstrap-icons';
-import '../styles/twitter_bootstrap.scss';
-import '../styles/layout.scss';
-
-require.context('../images', true);
