8d48e4044572d0739a5306bd0d59ec5c6810c427
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-20 22:14:46 +0000

    feat(devise): adding user table db, db seeding, and implementing login (!3) Merge branch 'feat/add-login' into 'main' with the following commits: * feat: db user seeding and sign up removal
    * feat(sign_up): password compromisation and secure passwords from lab
    
    * feat(sign_up): changing sign up to create accounts to populate DB (as there is no sign up now)
    
    * fix: haml and rubocop linting issues
    
    * feat: creating haml devise views
    
    * feat: adding further security to devise and following standards
    
    * feat: user roles added to schema
    
    * feat: sign out
    
    * feat: Setting up devise and login.

---
+++ app/models/user.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unlock_token           :string
+#  user_role              :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  validate :password_complexity
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+    # :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable,
+    :lockable,
+    :trackable,
+    :timeoutable,
+    :pwned_password
+
+  enum user_role: { reporter: "Reporter", admin: "Admin" }
+
+  # Validates the complexity of a password
+  def password_complexity
+    if encrypted_password_changed? && password !~ /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+      errors.add(:password, "must contain upper and lower-case letters and numbers")
+    end
+  end
+end
