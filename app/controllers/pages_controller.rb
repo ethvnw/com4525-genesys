57427213f0fe68d967ee7b5e3d9d39d563c0b473
Jack Burke <jburke5@sheffield.ac.uk>
2024-11-22 15:25:50 +0000

    fix(navbar): fixed login/logout functionality on navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@
 # A basic controller from the template app
 class PagesController < ApplicationController
   def home
+    @script_packs = %w[application]
+    @style_packs = %w[application]
   end
 end
