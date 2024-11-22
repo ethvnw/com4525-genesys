a2b2c7756c6529f437d16f0785bf20dc266a2f0c
Jack Burke <jburke5@sheffield.ac.uk>
2024-11-22 14:46:13 +0000

    fix(eslint): resolved eslint issues

--- app/packs/scripts/home_carousels.js
+++ app/packs/scripts/home_carousels.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import Swiper from 'swiper/bundle';
 
 const carouselConfig = {
@@ -13,14 +14,16 @@ const carouselConfig = {
     el: '.swiper-scrollbar',
   },
   breakpoints: {
-    480: { slidesPerView: 2 },  // xsm
-    576: { slidesPerView: 2 },  // sm
-    768: { slidesPerView: 3 },  // md
-    992: { slidesPerView: 3 },  // lg
+    480: { slidesPerView: 2 }, // xsm
+    576: { slidesPerView: 2 }, // sm
+    768: { slidesPerView: 3 }, // md
+    992: { slidesPerView: 3 }, // lg
     1200: { slidesPerView: 4 }, // xl
     1400: { slidesPerView: 5 }, // xxl
-  }
+  },
 };
 
+/* eslint-disable no-unused-vars */
+/* Carousels */
 const featuresCarousel = new Swiper('.features-carousel', carouselConfig);
 const reviewsCarousel = new Swiper('.reviews-carousel', carouselConfig);
