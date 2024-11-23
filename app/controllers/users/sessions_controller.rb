a577080f3ddfdf8e3b6057e5148bc174ce445bcd
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-23 14:43:18 +0000

    feat(mailer): custom mailer to distinguish password reset emails and new staff password emails

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,8 +11,13 @@ module Users
         flash[:notice] = "Account #{user.email} is already registered."
       else
         # If the user doesn't exist, send a magic password link
-        user = User.create(email: params[:email], password: "BobSteven#*1", user_role: params[:user_role]) # SecureRandom.hex(10)
-        user.send_reset_password_instructions
+        user = User.create(email: params[:email], password: "BobSteven#*1", user_role: params[:user_role])
+
+        # Create a token (like reset password) so the user can change their password
+        raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+        user.update(reset_password_token: hashed, reset_password_sent_at: Time.now.utc)
+        CustomDeviseMailer.send_magic_password_instructions(user, raw).deliver_later
+
         flash[:notice] = "Account created and email sent to #{user.email}."
       end
 
