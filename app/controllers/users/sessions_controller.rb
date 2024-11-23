f1d03feaa2ff36554e3a410d032d61c2544596d2
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-23 12:15:41 +0000

    feat(magic-passwords): allow user creation and password reset email in one step

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Users
+  # Handles the usage of magic password links
+  class SessionsController < Devise::SessionsController
+    def send_magic_link
+      # Find the user by email to see if they already exist
+      user = User.find_by(email: params[:email])
+
+      if user
+        flash[:notice] = "Account #{user.email} is already registered."
+      else
+        # If the user doesn't exist, send a magic password link
+        user = User.create(email: params[:email], password: "BobSteven#*1") # SecureRandom.hex(10)
+        user.send_reset_password_instructions
+        flash[:notice] = "Account created and email sent to #{user.email}."
+      end
+
+      redirect_to(new_user_session_path)
+    end
+  end
+end
