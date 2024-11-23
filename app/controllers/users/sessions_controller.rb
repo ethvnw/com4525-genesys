2081ad1530709e1c867d05f5c7278a68791bb520
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-23 17:16:41 +0000

    feat: replacing default insecure password for staff generated accounts

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,12 +5,15 @@ module Users
   class SessionsController < Devise::SessionsController
     def send_magic_link
       user = User.find_by(email: params[:email])
+      # Create a random password (#X) is to ensure it contains a capital and symbol to pass validations
+      # Param 48 encodes 64 characters + 2 -> 66 passing validation
+      secure_random = SecureRandom.urlsafe_base64(48) + "#X"
 
       if user
         flash[:notice] = "Account #{user.email} is already registered."
       else
         # If the user doesn't exist, send a magic password link
-        user = User.create(email: params[:email], password: "BobSteven#*1", user_role: params[:user_role])
+        user = User.create(email: params[:email], password: secure_random, user_role: params[:user_role])
 
         # Create a token (like reset password) so the user can change their password
         raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
