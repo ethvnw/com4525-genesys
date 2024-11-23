527dbc882532fec1d675b9c5997ec16919ed712b
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-23 12:44:24 +0000

    feat(unauthorised-access): prevent error pages when visiting unauthorised access pages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,13 +5,13 @@ module Users
   class SessionsController < Devise::SessionsController
     def send_magic_link
       # Find the user by email to see if they already exist
-      user = User.find_by(email: params[:email])
+      user = User.find_by(email: params[:email], user_role: params[:user_role])
 
       if user
         flash[:notice] = "Account #{user.email} is already registered."
       else
         # If the user doesn't exist, send a magic password link
-        user = User.create(email: params[:email], password: "BobSteven#*1") # SecureRandom.hex(10)
+        user = User.create(email: params[:email], password: "BobSteven#*1", user_role: params[:user_role]) # SecureRandom.hex(10)
         user.send_reset_password_instructions
         flash[:notice] = "Account created and email sent to #{user.email}."
       end
