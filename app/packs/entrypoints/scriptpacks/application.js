ab145f333e215eb78932861cbfe48522522289db
Jack Burke <jburke5@sheffield.ac.uk>
2024-11-25 01:58:25 +0000

    feat(social-share): added offcanvas with clipboard, email, and social media sharing

--- app/packs/entrypoints/scriptpacks/application.js
+++ app/packs/entrypoints/scriptpacks/application.js
@@ -1,6 +1,6 @@
 import Rails from '@rails/ujs';
 import 'bootstrap';
 import '../../scripts/home_carousels';
-import '../../scripts/home_feature_share';
+import '../../scripts/home_feature_social_share';
 
 Rails.start();
