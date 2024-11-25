d2a9bffd8fea6e944b80524152bf9466b398fbbf
Ethan Watts <ethan.watts@live.co.uk>
2024-11-25 11:41:46 +0000

    feat(reviews): add max content length, fix wrap
    Co-authored-by: Ellis Barker <ebarker5@sheffield.ac.uk>

--- app/packs/scripts/home_like_reviews.js
+++ app/packs/scripts/home_like_reviews.js
@@ -16,11 +16,21 @@ likeButtons.forEach((button) => {
   if (reviewLiked === 'true') {
     button.classList.add('active');
     button.setAttribute('aria-pressed', 'true');
+    button.querySelector('i').classList.remove('bi-hand-thumbs-up');
+    button.querySelector('i').classList.add('bi-hand-thumbs-up-fill');
   }
 
   button.addEventListener('click', () => {
     const likeCount = button.querySelector('.like-count');
     const isLiked = localStorage.getItem(reviewId) === 'true';
+
+    if (isLiked) {
+      button.querySelector('i').classList.remove('bi-hand-thumbs-up-fill');
+      button.querySelector('i').classList.add('bi-hand-thumbs-up');
+    } else {
+      button.querySelector('i').classList.remove('bi-hand-thumbs-up');
+      button.querySelector('i').classList.add('bi-hand-thumbs-up-fill');
+    }
 
     likeCount.innerText = parseInt(likeCount.innerText, 10) + (isLiked ? -1 : 1);
     localStorage.setItem(reviewId, !isLiked);
