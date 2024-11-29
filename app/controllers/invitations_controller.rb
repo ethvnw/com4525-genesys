0a2fe4ffdf4ffbc83392407cd02a9ab1e71120c4
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-11-29 16:50:00 +0000

    fix(route): prevent access to devise invitable default inv route

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,6 +3,7 @@
 # Handles account creation invitations
 class InvitationsController < Devise::InvitationsController
   before_action :authorize_invitation, only: [:new, :create]
+  before_action :block_default_new_invitation_path
 
   def create
     self.resource = invite_resource
@@ -15,6 +16,13 @@ class InvitationsController < Devise::InvitationsController
   end
 
   private
+
+  # Prevent access to devise invitable's default invitation view
+  def block_default_new_invitation_path
+    if request.path == new_user_invitation_path
+      head(:not_found)
+    end
+  end
 
   def authorize_invitation
     authorize!(:invite, User)
