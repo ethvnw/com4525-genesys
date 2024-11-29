93ccc64774b5016d661cfca96c9339440ba52493
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-11-29 00:36:56 +0000

    fix(eslint): fixed eslint error in api_routes

--- app/packs/scripts/constants/api_routes.js
+++ app/packs/scripts/constants/api_routes.js
@@ -1,3 +1,3 @@
-const API_ROUTE = '/api';
+const API_BASE_ROUTE = '/api';
 
-export const getFeatureShareRoute = (featureId, method) => `${API_ROUTE}/features/${featureId}/share?method=${method}`;
+export const getFeatureShareRoute = (featureId, method) => `${API_BASE_ROUTE}/features/${featureId}/share?method=${method}`;
