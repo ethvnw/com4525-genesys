8e14c9358edeffa9c48d2970e94b787f60e0e636
James March <jmarch2@sheffield.ac.uk>
2024-11-29 17:52:34 +0000

    fix(faq-reviews): use same js files for reviews and faq ordering and visibility functionality (!19) Merge branch 'fix/faq-reviews-reusable-code' into 'main' with the following commits: * fix(faq): remove logger comment
    * fix(visibility): fix Firefox form error
    
    * fix(faq): broken path for ordering
    
    * fix(faq): removed todo from code that has now been resolved
    
    * fix(admin): lint issue
    
    * fix(admin): partials for faq and reviews dashboard
    
    * fix(faq): bug where order swaps would swap cards with modals instead of other cards
    
    * fix(faq): updated seeds to include order
    
    * fix(faq-reviews): partial for reused code for faq and review cards
    
    * fix(faq): remove first person comments
    
    * fix(faq): hard refresh issue
    
    * fix(faq): add home pack to faq page so interactivity works
    
    * fix(faq-reviews): fix invalid reference when saving new order
    
    * fix(faq-reviews): code comments for the js
    
    * fix(faq-reviews): faq and reviews now use same js files for order and visibility

---
+++ app/packs/scripts/admin_visibility.js
@@ -0,0 +1,63 @@
+/**
+ * Handles the visibility of questions in the admin panel.
+ */
+
+const visibilityUpdateForms = document.querySelectorAll('form.update-visibility');
+// Get the containers and counts for visible and hidden items to update later
+const visibleContainer = document.getElementById('visible-items');
+const hiddenContainer = document.getElementById('hidden-items');
+const visibleCount = document.getElementById('visible-count');
+const hiddenCount = document.getElementById('hidden-count');
+
+// When an item is given visibility, update the pre-existing order of the visible items
+const updateVisibleItemOrders = (hiddenItemOrder) => {
+  const items = visibleContainer.querySelectorAll('.item');
+  items.forEach((item) => {
+    if (item.getAttribute('data-order') > hiddenItemOrder) {
+      item.setAttribute('data-order', item.getAttribute('data-order') - 1);
+    }
+  });
+  document.getElementById('save-form').dispatchEvent(new Event('submit', { cancelable: true }));
+};
+
+visibilityUpdateForms.forEach((form) => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const item = form.closest('.item');
+    const isVisible = item.parentElement === visibleContainer;
+
+    // If the item is visible, update the order of the visible items
+    if (isVisible) {
+      updateVisibleItemOrders(item.getAttribute('data-order'));
+    }
+
+    item.setAttribute('data-order', isVisible ? 0 : visibleContainer.children.length);
+
+    const formData = new FormData(form);
+    formData.append('order', item.getAttribute('data-order'));
+
+    // When submitting the form, update the visibility of the item and its eye-icon
+    fetch(form.action, {
+      method: 'POST',
+      headers: {
+        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+      },
+      body: formData,
+    }).then(() => {
+      const targetContainer = isVisible ? hiddenContainer : visibleContainer;
+      // Change the eye-icon to the opposite of what it was
+      const eyeIcon = item.querySelector(isVisible ? '.bi-eye-slash-fill' : '.bi-eye-fill');
+
+      // Update the visible and hidden counts based on the visibility of the item
+      visibleCount.innerText = parseInt(visibleCount.innerText, 10) + (isVisible ? -1 : 1);
+      hiddenCount.innerText = parseInt(hiddenCount.innerText, 10) + (isVisible ? 1 : -1);
+
+      // Move the item to the correct container
+      item.remove();
+      item.querySelector('.order-arrows').classList.toggle('d-none', isVisible);
+      targetContainer.appendChild(item);
+      eyeIcon.classList.toggle('bi-eye-fill', isVisible);
+      eyeIcon.classList.toggle('bi-eye-slash-fill', !isVisible);
+    });
+  });
+});
