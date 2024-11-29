005118e835508c9b42e569056678924d980df8e6
Jack Burke <jburke5@sheffield.ac.uk>
2024-11-29 15:36:15 +0000

    fix(rubocop): fixed rubocop issues

--- spec/models/subscription_tier_spec.rb
+++ spec/models/subscription_tier_spec.rb
@@ -15,21 +15,21 @@
 require "rails_helper"
 
 RSpec.describe(SubscriptionTier, type: :model) do
-  describe '#premium_subscription?' do
+  describe "#premium_subscription?" do
     let(:premium_tier) { build(:subscription_tier, price_gbp: 10) }
     let(:free_tier) { build(:subscription_tier, price_gbp: 0) }
     let(:no_subscription_tier) { build(:subscription_tier, price_gbp: nil) }
 
-    it 'returns true if price_gbp is greater than 0' do
-      expect(premium_tier.premium_subscription?).to be_truthy
+    it "returns true if price_gbp is greater than 0" do
+      expect(premium_tier.premium_subscription?).to(be_truthy)
     end
 
-    it 'returns false if price_gbp is 0' do
-      expect(free_tier.premium_subscription?).to be_falsey
+    it "returns false if price_gbp is 0" do
+      expect(free_tier.premium_subscription?).to(be_falsey)
     end
 
-    it 'returns false if price_gbp is nil' do
-      expect(no_subscription_tier.premium_subscription?).to be_falsey
+    it "returns false if price_gbp is nil" do
+      expect(no_subscription_tier.premium_subscription?).to(be_falsey)
     end
   end
 end
