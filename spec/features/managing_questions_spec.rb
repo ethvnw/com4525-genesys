aa2aec5b95d9591f0cf8b7e24cc72e023da0fc69
James <jameswmarch2002@gmail.com>
2024-11-30 12:20:43 +0000

    test(faq): added tests for visibility and answering questions

--- spec/features/managing_questions_spec.rb
+++ spec/features/managing_questions_spec.rb
@@ -66,4 +66,66 @@ RSpec.feature("Managing questions") do
       expect(page).to(have_content("A hidden question"))
     end
   end
+
+  specify "I can set a visible question to hidden" do
+    question = create(:question, question: "A visible question", is_hidden: false)
+    login_as(admin, scope: :user)
+    visit "/admin/manage_questions"
+    within("#hidden-items") do
+      expect(page).not_to(have_content("A visible question"))
+    end
+    within("#visible-items") do
+      expect(page).to(have_content("A visible question"))
+      find("#visibility-toggle-#{question.id}").click
+    end
+    # Capybara deals with the request weirdly, so the page is refereshed to reload the content
+    visit "/admin/manage_questions"
+    within("#visible-items") do
+      expect(page).not_to(have_content("A visible question"))
+    end
+    within("#hidden-items") do
+      expect(page).to(have_content("A visible question"))
+    end
+  end
+
+  specify "A visible question will appear on the FAQ page" do
+    create(:question, question: "A visible question", is_hidden: false)
+    visit "/faq/"
+    expect(page).to(have_content("A visible question"))
+  end
+
+  specify "A hidden question will not appear on the FAQ page" do
+    create(:question, question: "A hidden question", is_hidden: true)
+    visit "/faq/"
+    expect(page).not_to(have_content("A hidden question"))
+  end
+
+  specify "I can answer a question and it will show on the admin FAQ dashboard" do
+    question = create(:question, question: "A question")
+    login_as(admin, scope: :user)
+    visit "/admin/manage_questions"
+    # Click on the answer button to make the modal appear
+    within("#visible-items") do
+      click_on "Answer"
+    end
+    fill_in "answer_#{question.id}", with: "An answer"
+    click_on "Submit Answer"
+    within("#item_#{question.id}") do
+      expect(page).to(have_content("An answer"))
+    end
+  end
+
+  specify "I can answer a question and it will show on the FAQ page" do
+    question = create(:question, question: "A question", is_hidden: false)
+    login_as(admin, scope: :user)
+    visit "/admin/manage_questions"
+    # Click on the answer button to make the modal appear
+    within("#visible-items") do
+      click_on "Answer"
+    end
+    fill_in "answer_#{question.id}", with: "An answer"
+    click_on "Submit Answer"
+    visit "/faq/"
+    expect(page).to(have_content("An answer"))
+  end
 end
