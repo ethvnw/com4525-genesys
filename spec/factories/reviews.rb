30af545f4fc55eec343a000990847cd6f8fc80f1
Ellis Barker <ebarker5@sheffield.ac.uk>
2024-12-02 13:50:38 +0000

    test(reviews): feature tests for review functionality (submission, viewing, ordering, visibility) (!24) Merge branch 'test/9-13-reviews' into 'main' with the following commits: * test(reviews): fixing log-ins for specific tests and FactoryBot definition
    * test(reviews): cleaned up FactoryBot usage
    
    * fix(tests): readded the top/bottom review arrow-related tests after modifying css
    
    * feat(subscriptions-page-app-features): add app features, subscription pricing and register pages (!23)
    Merge branch 'feat/7-8-14-pricing-and-registration' into 'main' with the following commits:
    * fix(PR): fixed issues request in PR
    
    * fix(pages_controller): fixed duplicate route in controllers
    
    * feat(testing): testing for decorators
    
    * feat(pricing-page-testing): added testing for pricing page
    
    * fix(rubocop): fixed rubocop issues
    
    * fix(factories): cleaned up factories
    
    * feat(testing): deleted not needed tests, added tests for subscriptions
    
    * fix(pipeline): fixed branch to pass pipeline
    
    * feat(pricing-and-landing-page): seeded data for pricing and landing pages
    
    * feat(register-page): added register page
    
    * fix(login): fixed issues with height on login page
    
    * fix(css): fix main content height bug
    
    * fix(rubocop): fixing issue with rubocop
    
    * feat(register): added skeleton for register page
    
    * fix(rubocop): fixed issue with rubocop
    
    * feat(landing-page): refactored views into partials
    
    * feat(app-features): added app features and linker table to subscriptions
    
    * feat(pricing-page): added pricing images and css
    
    * feat(subscription-tiers): added subscription tiers to database, with subscription tiers page
    
    * feat(navbar): navbar refactoring and use cancancan
    
    * feat(pricing-page): initial set up of pricing page route
    
    * test(reviews): extra feature tests for boundary cases
    
    * test(reviews): feature tests for review reordering
    
    * test(reviews): rubocop format fix
    
    * test(reviews): feature tests for review visibility
    
    * test(reviews): rubocop format fix
    
    * test(reviews): fixed format for feature tests, like button tests
    
    Co-authored-by: Ethan Watts <eawatts1@sheffield.ac.uk>
    
    * test(reviews): fixed formatting of review feature tests, TODO: visibility, engagement, ordering

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                 :bigint           not null, primary key
+#  content            :text
+#  engagement_counter :integer          default(0)
+#  is_hidden          :boolean          default(TRUE)
+#  name               :string(50)
+#  order              :integer          default(0)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+FactoryBot.define do
+  factory :review do
+    name { "MyName" }
+    content { "MyContent" }
+    order { 0 }
+    is_hidden { false }
+    engagement_counter { 0 }
+  end
+end
