1cd002373e16a6524f77f168796fbc126ae145f3
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-12-02 15:50:32 +0000

    feat(db): added registrations table to database

---
+++ app/models/registration.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: registrations
+#
+#  id                   :bigint           not null, primary key
+#  country_code         :string
+#  email                :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  subscription_tier_id :bigint           not null
+#
+# Indexes
+#
+#  index_registrations_on_subscription_tier_id  (subscription_tier_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (subscription_tier_id => subscription_tiers.id)
+#
+class Registration < ApplicationRecord
+end
