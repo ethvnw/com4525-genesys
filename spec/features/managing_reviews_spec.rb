caae00fbac040b27122a0d337bb3f3601657d72a
Ellis Barker <ebarker5@sheffield.ac.uk>
2024-12-02 13:50:38 +0000

    test(reviews): feature tests for review functionality (submission, viewing, ordering, visibility) (!24) Merge branch 'test/9-13-reviews' into 'main' with the following commits: * test(reviews): fixing log-ins for specific tests and FactoryBot definition
    * test(reviews): cleaned up FactoryBot usage
    
    * fix(tests): readded the top/bottom review arrow-related tests after modifying css
    
    * feat(subscriptions-page-app-features): add app features, subscription pricing and register pages (!23)
    Merge branch 'feat/7-8-14-pricing-and-registration' into 'main' with the following commits:
    * fix(PR): fixed issues request in PR
    
    * fix(pages_controller): fixed duplicate route in controllers
    
    * feat(testing): testing for decorators
    
    * feat(pricing-page-testing): added testing for pricing page
    
    * fix(rubocop): fixed rubocop issues
    
    * fix(factories): cleaned up factories
    
    * feat(testing): deleted not needed tests, added tests for subscriptions
    
    * fix(pipeline): fixed branch to pass pipeline
    
    * feat(pricing-and-landing-page): seeded data for pricing and landing pages
    
    * feat(register-page): added register page
    
    * fix(login): fixed issues with height on login page
    
    * fix(css): fix main content height bug
    
    * fix(rubocop): fixing issue with rubocop
    
    * feat(register): added skeleton for register page
    
    * fix(rubocop): fixed issue with rubocop
    
    * feat(landing-page): refactored views into partials
    
    * feat(app-features): added app features and linker table to subscriptions
    
    * feat(pricing-page): added pricing images and css
    
    * feat(subscription-tiers): added subscription tiers to database, with subscription tiers page
    
    * feat(navbar): navbar refactoring and use cancancan
    
    * feat(pricing-page): initial set up of pricing page route
    
    * test(reviews): extra feature tests for boundary cases
    
    * test(reviews): feature tests for review reordering
    
    * test(reviews): rubocop format fix
    
    * test(reviews): feature tests for review visibility
    
    * test(reviews): rubocop format fix
    
    * test(reviews): fixed format for feature tests, like button tests
    
    Co-authored-by: Ethan Watts <eawatts1@sheffield.ac.uk>
    
    * test(reviews): fixed formatting of review feature tests, TODO: visibility, engagement, ordering

--- spec/features/managing_reviews_spec.rb
+++ spec/features/managing_reviews_spec.rb
@@ -3,32 +3,209 @@
 require "rails_helper"
 
 RSpec.feature("Managing reviews") do
-  specify "I can add a review" do
-    visit "/"
-    fill_in "review_name", with: "Test User"
-    fill_in "review_content", with: "Content for the review"
-    click_on "Submit Review"
-    expect(page).not_to(have_content("Content for the review"))
+  feature "Submitting reviews" do
+    scenario "I cannot submit a review with no content" do
+      visit root_path
+      fill_in "review_name", with: "Test User"
+      click_on "Submit Review"
+      expect(page).to(have_content("Content can't be blank"))
+    end
+
+    scenario "I cannot submit a review with too much content (>400 characters)" do
+      visit root_path
+      fill_in "review_name", with: "Test User"
+      fill_in "review_content", with: "a" * 401
+      click_on "Submit Review"
+      expect(page).to(have_content("Content is too long (maximum is 400 characters)"))
+    end
+
+    scenario "I cannot submit a review with no name" do
+      visit root_path
+      fill_in "review_content", with: "Content for the review"
+      click_on "Submit Review"
+      expect(page).to(have_content("Name can't be blank"))
+    end
+
+    scenario "I cannot submit a review with too long of a name (>50 characters)" do
+      visit root_path
+      fill_in "review_name", with: "a" * 51
+      fill_in "review_content", with: "Test Content"
+      click_on "Submit Review"
+      expect(page).to(have_content("Name is too long (maximum is 50 characters)"))
+    end
+
+    scenario "I cannot submit a review with no name or content" do
+      visit root_path
+      click_on "Submit Review"
+      expect(page).to(have_content("Name can't be blank"))
+      expect(page).to(have_content("Content can't be blank"))
+    end
+
+    scenario "I can submit a review and not see it yet (as it is hidden until an admin unhides it)" do
+      visit root_path
+      fill_in "review_name", with: "Test User"
+      fill_in "review_content", with: "Content for the review"
+      click_on "Submit Review"
+      within(:css, ".reviews-carousel") do
+        expect(page).not_to(have_content("Content for the review"))
+      end
+    end
   end
 
-  specify "I cannot add a review with no content" do
-    visit "/"
-    fill_in "review_name", with: "Test User"
-    click_on "Submit Review"
-    expect(page).to(have_content("Content can't be blank"))
+  feature "Liking reviews" do
+    given!(:review) { FactoryBot.create(:review) }
+
+    scenario "I can like a review" do
+      visit root_path
+      within("div.reviews-carousel") do
+        find("button#review_#{review.id}").click
+      end
+      visit root_path
+      within("div.reviews-carousel") do
+        expect(page).to(have_content("1"))
+      end
+    end
+
+    scenario "I can unlike a review" do
+      review.increment!(:engagement_counter)
+      visit root_path
+      within("div.reviews-carousel") do
+        find("button#review_#{review.id}").click
+      end
+      visit root_path
+      within("div.reviews-carousel") do
+        expect(page).to(have_content("0"))
+      end
+    end
+
+    scenario "I can like and unlike a review", js: true do
+      visit root_path
+      within("div.reviews-carousel") do
+        find("button#review_#{review.id}").click
+        find("button#review_#{review.id}").click
+      end
+      visit root_path
+      within("div.reviews-carousel") do
+        expect(page).to(have_content("0"))
+      end
+    end
   end
 
-  specify "I cannot add a review with no name" do
-    visit "/"
-    fill_in "review_content", with: "Content for the review"
-    click_on "Submit Review"
-    expect(page).to(have_content("Name can't be blank"))
+  let(:admin) { create(:admin) }
+
+  feature "Seeing a review in the admin review management system" do
+    scenario "I can submit a review and only the admin can see it through the review management system" do
+      visit root_path
+      fill_in "review_name", with: "Test User"
+      fill_in "review_content", with: "Content for the review"
+      click_on "Submit Review"
+      login_as(admin, scope: :user)
+      visit admin_manage_reviews_path
+      within(:css, "#hidden-items") do
+        expect(page).to(have_content("Content for the review"))
+      end
+    end
   end
 
-  specify "I cannot add a review with no name or content" do
-    visit "/"
-    click_on "Submit Review"
-    expect(page).to(have_content("Name can't be blank"))
-    expect(page).to(have_content("Content can't be blank"))
+  feature "Edit review visibility" do
+    before do
+      login_as(admin, scope: :user)
+    end
+
+    given!(:review) { FactoryBot.create(:review) }
+
+    scenario "I can make a review hidden and no longer see it on the home page" do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items form.update-visibility") do
+        find("button").click
+      end
+      visit root_path
+      within("div.reviews-carousel") do
+        expect(page).not_to(have_content(review.content))
+      end
+    end
+
+    scenario "I can make a review visible and see it on the home page" do
+      review.toggle!(:is_hidden)
+      visit admin_manage_reviews_path
+      within(:css, "#hidden-items form.update-visibility") do
+        find("button").click
+      end
+      visit root_path
+      within("div.reviews-carousel") do
+        expect(page).to(have_content(review.content))
+      end
+    end
+  end
+
+  feature "Editing review orders" do
+    before do
+      login_as(admin, scope: :user)
+    end
+
+    given!(:review1) { FactoryBot.create(:review) }
+    given!(:review2) { FactoryBot.create(:review, name: "OtherName", content: "OtherContent", order: 1) }
+    given!(:review3) do
+      FactoryBot.create(:review, name: "HiddenName", content: "HiddenContent", is_hidden: true, order: 2)
+    end
+
+    scenario "I can move a review closer to the front so it appears before other reviews on the home page", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items #item_#{review2.id} .order-arrows") do
+        find(".order-up-arrow").click
+      end
+      click_on "Save Changes"
+      visit root_path
+      within(:css, ".reviews-carousel .swiper-wrapper") do
+        first_review = find('[data-swiper-slide-index="0"]')
+        expect(first_review).to(have_content(review2.content))
+      end
+    end
+
+    scenario "I can move a review closer to the end so it appears after other reviews on the home page", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items #item_#{review1.id} .order-arrows") do
+        find(".order-down-arrow").click
+      end
+      click_on "Save Changes"
+      visit root_path
+      within(:css, ".reviews-carousel .swiper-wrapper") do
+        second_review = find('[data-swiper-slide-index="1"]')
+        expect(second_review).to(have_content(review1.content))
+      end
+    end
+
+    scenario "I cannot move the first review even closer to the front as there is no up arrow shown", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items #item_#{review1.id} .order-arrows") do
+        expect(page).not_to(have_css(".order-up-arrow"))
+      end
+    end
+
+    scenario "I cannot move the last review even closer to the end as there is no down arrow", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items #item_#{review2.id} .order-arrows") do
+        expect(page).not_to(have_css(".order-down-arrow"))
+      end
+    end
+
+    scenario "I cannot edit the order of a hidden review as there are no arrows present", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#hidden-items #item_#{review3.id}") do
+        expect(page).not_to(have_css(".order-arrows"))
+      end
+    end
+
+    scenario "I can discard changes to order by not clicking the 'Save Changes' button", js: true do
+      visit admin_manage_reviews_path
+      within(:css, "#visible-items #item_#{review2.id} .order-arrows") do
+        find(".order-up-arrow").click
+      end
+      visit root_path
+      within(:css, ".reviews-carousel .swiper-wrapper") do
+        first_review = find('[data-swiper-slide-index="0"]')
+        expect(first_review).to(have_content(review1.content))
+      end
+    end
   end
 end
