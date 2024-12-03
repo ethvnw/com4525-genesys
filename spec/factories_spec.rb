0333fcf9679ffa811213f61f4f95df04aaa09e6a
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-12-03 11:38:14 +0000

    test(factories): moved James' factorybot tests to factories_spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -5,10 +5,24 @@
 
 require "rails_helper"
 
-FactoryBot.factories.map(&:name).each do |factory_name|
-  describe "The #{factory_name} factory" do
-    it "is valid" do
-      expect(build(factory_name)).to(be_valid)
+RSpec.describe(FactoryBot) do
+  FactoryBot.factories.map(&:name).each do |factory_name|
+    describe "the #{factory_name} factory" do
+      it "is valid" do
+        expect(build(factory_name)).to(be_valid)
+      end
+    end
+  end
+
+  describe "the admin factory" do
+    it "creates an admin user" do
+      expect(build(:admin).admin?).to(be_truthy)
+    end
+  end
+
+  describe "the reporter factory" do
+    it "creates a reporter user" do
+      expect(build(:reporter).reporter?).to(be_truthy)
     end
   end
 end
