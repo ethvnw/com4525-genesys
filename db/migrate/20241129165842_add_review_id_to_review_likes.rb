be18190df04ce31679dce5d5160342c08d3ba9f5
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-12-04 16:56:43 +0000

    feat(db): added linker tables for landing page journey

--- db/migrate/20241129165842_add_review_id_to_review_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddReviewIdToReviewLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :review_likes, :review, foreign_key: true
-  end
-end
--- db/migrate/20241204165122_create_feature_shares.rb
+++ db/migrate/20241204165122_create_feature_shares.rb
@@ -1,8 +1,12 @@
 class CreateFeatureShares < ActiveRecord::Migration[7.0]
   def change
     create_table :feature_shares do |t|
-      t.string(:media)
-      t.datetime(:timestamp)
+      t.string :media
+
+      t.timestamps
+
+      t.belongs_to :registration
+      t.belongs_to :app_feature
     end
   end
 end
