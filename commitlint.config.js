cf17d496c4bd51c23b5cd4e752cde14e8c8f5154
Jack Sanders <jsanders4@sheffield.ac.uk>
2024-12-05 13:47:37 +0000

    fix(deploy): updated CI to work with deploy

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    // Don't lint deploy messages
+    (message) => /Bumped to version \d+ \[skip ci\]/.test(message),
+  ],
+};
