a61963ff2e93e4292b5d6c19d97289e75e02b933
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2024-12-05 11:07:32 +0000

    chore: removed unsused code

--- app/controllers/reporter/dashboard_controller.rb
+++ app/controllers/reporter/dashboard_controller.rb
@@ -8,14 +8,5 @@ module Reporter
 
     def index
     end
-
-    private
-
-    def authorize_reporter
-      unless can?(:access, :reporter_dashboard)
-        flash[:alert] = "Unauthorized Access."
-        redirect_to(root_path)
-      end
-    end
   end
 end
