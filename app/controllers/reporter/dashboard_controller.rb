6ac980f91e61ef5c4c98b38955ba9ba2b1d31c33
Jack Burke <jburke5@sheffield.ac.uk>
2024-12-06 03:07:05 +0000

    feat(reporter-dashboard): added landing page statistics

--- app/controllers/reporter/dashboard_controller.rb
+++ app/controllers/reporter/dashboard_controller.rb
@@ -4,11 +4,19 @@ module Reporter
   # Dashboard controller
   class DashboardController < Reporter::BaseController
     def index
+      # Statistics for registrations
       @registrations_today = Registration.by_day[Time.current.beginning_of_day]
       @registrations_this_week = Registration.by_week[Time.current.beginning_of_week]
       @registrations_this_month = Registration.by_month[Time.current.beginning_of_month]
       @registrations_all_time = Registration.count
       @registrations_by_country = Registration.by_country
+
+      # Statistics for landing page visits
+      @landing_page_visits_today = LandingPageVisit.by_day[Time.current.beginning_of_day]
+      @landing_page_visits_this_week = LandingPageVisit.by_week[Time.current.beginning_of_week]
+      @landing_page_visits_this_month = LandingPageVisit.by_month[Time.current.beginning_of_month]
+      @landing_page_visits_all_time = LandingPageVisit.count
+      @landing_page_visits_by_country = LandingPageVisit.by_country
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_04_165550) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_06_025348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_04_165550) do
     t.bigint "app_feature_id"
     t.index ["app_feature_id"], name: "index_feature_shares_on_app_feature_id"
     t.index ["registration_id"], name: "index_feature_shares_on_registration_id"
+  end
+
+  create_table "landing_page_visits", force: :cascade do |t|
+    t.string "country_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "question_clicks", force: :cascade do |t|
