5e44617d7776314a325cf49536b9ed71e36d1d24
Jack Burke <jburke5@sheffield.ac.uk>
2024-12-06 03:18:17 +0000

    feat(app-feature-engagement): added engagement counter to app feature

--- spec/factories/app_features.rb
+++ spec/factories/app_features.rb
@@ -4,11 +4,12 @@
 #
 # Table name: app_features
 #
-#  id          :bigint           not null, primary key
-#  description :text
-#  name        :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :bigint           not null, primary key
+#  description        :text
+#  engagement_counter :integer          default(0), not null
+#  name               :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
 FactoryBot.define do
   factory :app_feature do
