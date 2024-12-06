d0a6e1f85db6baeb86416225f05fe39d0907d004
Jack Burke <jburke5@sheffield.ac.uk>
2024-12-06 01:14:34 +0000

    feat(reporter-dashboard): added statistics for registrations

--- app/controllers/reporter/dashboard_controller.rb
+++ app/controllers/reporter/dashboard_controller.rb
@@ -4,6 +4,11 @@ module Reporter
   # Dashboard controller
   class DashboardController < Reporter::BaseController
     def index
+      @registrations_today = Registration.by_day[Time.current.beginning_of_day]
+      @registrations_this_week = Registration.by_week[Time.current.beginning_of_week]
+      @registrations_this_month = Registration.by_month[Time.current.beginning_of_month]
+      @registrations_all_time = Registration.count
+      @registrations_by_country = Registration.by_country
     end
   end
 end
