e1a4c887b7cd7ea99bd0d4bdcde8b3c84bcce061
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-03-10 00:10:59 +0000

    feat(forms): finished form builder and updated registration form to use it

--- app/packs/entrypoints/scriptpacks/application.js
+++ app/packs/entrypoints/scriptpacks/application.js
@@ -1,6 +1,8 @@
 import Rails from '@rails/ujs';
 import 'bootstrap';
+import { Turbo } from '@hotwired/turbo-rails';
 
 import '../../scripts/page_alert_driver';
 
+Turbo.session.drive = false;
 Rails.start();
