7d77195939b4ed298650442e2e815eb094947444
Ethan Watts <ethan.watts@live.co.uk>
2025-03-12 14:37:43 +0000

    fix: vcr capturing local requests on gitlab

--- spec/services/sharing/facebook_sharer_spec.rb
+++ spec/services/sharing/facebook_sharer_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe("Sharing::FacebookSharer") do
   let(:feature) { build(:app_feature) }
 
-  it "Correctly formats the feature details as a facebook sharing link" do
+  it "Correctly formats the feature details as a facebook sharing link", vcr: true do
     facebook = Sharing::FacebookSharer.call(feature)
 
     expect(facebook).to(start_with("https://www.facebook.com"))
