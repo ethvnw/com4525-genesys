6d1b338552b89bf4a0bc4746b0b1c0db329acd50
Ethan Watts <ethan.watts@live.co.uk>
2025-03-12 14:37:43 +0000

    fix: vcr capturing local requests on gitlab

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -7,4 +7,6 @@ VCR.configure do |c|
   c.cassette_library_dir = "vcr_cassettes"
   c.hook_into(:webmock)
   c.configure_rspec_metadata!
+  c.default_cassette_options = { record: :new_episodes }
+  c.allow_http_connections_when_no_cassette = true
 end
