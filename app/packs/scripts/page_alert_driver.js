e47fbf9f1a28b7cdbe83bc2637a1cf9790fd8a3d
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-03-14 10:52:37 +0000

    fix(toasts): turbo now reloads toasts

--- app/packs/scripts/page_alert_driver.js
+++ app/packs/scripts/page_alert_driver.js
@@ -1,9 +1,29 @@
 import { Toast } from 'bootstrap';
+import buildNewToast from './lib/ToastUtils';
 
-// Check for toasts every time a turbo pageload occurs
-document.addEventListener('turbo:load', () => {
-  // Show all pre-filled toasts
-  Array.from(document.getElementsByClassName('toast')).forEach((e) => {
-    new Toast(e).show();
+const toastContainer = document.getElementById('toast-list');
+
+// Show all pre-filled toasts
+Array.from(document.getElementsByClassName('toast')).forEach((e) => {
+  new Toast(e).show();
+});
+
+// Check for toasts every time a turbo frame loads
+document.addEventListener('turbo:frame-load', () => {
+  // Delete old toasts
+  Array.from(toastContainer.children).forEach((elem) => {
+    if (!elem.classList.contains('show')) {
+      toastContainer.removeChild(elem);
+    }
+  });
+
+  document.querySelectorAll('[data-turbo-toast]').forEach((turboToastContainer) => {
+    if (!Object.prototype.hasOwnProperty.call(turboToastContainer.dataset, 'notifications')) {
+      return false;
+    }
+
+    JSON.parse(turboToastContainer.dataset.notifications).forEach((notification) => {
+      buildNewToast(notification.message, notification.notification_type).show();
+    });
   });
 });
