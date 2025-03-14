dea97a5c2b9e0e015ef1cc2b4bb034dae47d8bbf
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-03-14 14:30:14 +0000

    feat(forms): updated invite form

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,6 +6,16 @@ module Admin
     def index
       @users = User.where.not(id: current_user.id).decorate
       @registrations = Registration.all.order(created_at: :desc).decorate
+
+      # Variables for invitation form
+      @user = if session[:user_data]
+        User.new(session[:user_data])
+      else
+        User.new
+      end
+
+      puts @user.inspect
+      @errors = flash[:errors]
     end
   end
 end
