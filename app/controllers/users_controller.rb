3c5ef314f383bf2ffd9ce9680f6a29734966e743
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2025-03-20 15:12:46 +0000

    feat(avatar): update without passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
+# Handles updating user avatar
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def destroy_avatar
     user = User.find(params[:id])
     user.avatar.purge_later
-    redirect_to(edit_user_registration_path, notice: "Avatar successfully removed.")
+    redirect_to(edit_user_registration_path, notice: "Profile photo successfully removed.")
+  end
+
+  def update_avatar
+    if current_user.update(avatar_params)
+      redirect_to(edit_user_registration_path, notice: "Profile photo updated successfully.")
+    else
+      redirect_to(edit_user_registration_path, alert: "Failed to update profile photo.")
+    end
+  end
+
+  private
+
+  def avatar_params
+    params.require(:user).permit(:avatar)
   end
 end
