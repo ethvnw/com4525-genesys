2f6fe9295b93a498fc28a69dbaef769416e170e0
James <jameswmarch2002@gmail.com>
2025-03-23 10:27:09 +0000

    feat(avatar): preview image updates when image is selected

--- app/packs/scripts/user_add_avatar.js
+++ app/packs/scripts/user_add_avatar.js
@@ -3,6 +3,7 @@
 const browseButton = document.getElementById('avatar-input');
 const customButton = document.getElementById('avatar-custom');
 const fileNameSpan = document.getElementById('avatar-file-name');
+const avatarPreview = document.getElementById('avatar-preview');
 
 // When the custom button is clicked, trigger a click on the browse button
 customButton.addEventListener('click', () => {
@@ -11,6 +12,21 @@ customButton.addEventListener('click', () => {
 
 // Update the file name span when a file is selected
 browseButton.addEventListener('change', () => {
-  const fileName = browseButton.files.length > 0 ? browseButton.files[0].name : 'No file selected';
-  fileNameSpan.textContent = fileName;
+  if (browseButton.files.length > 0) {
+    // Make sure file is an image
+    if (browseButton.files[0].type.includes('image')) {
+      const file = browseButton.files[0].name;
+      fileNameSpan.textContent = file;
+      // Update the avatar preview
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        avatarPreview.src = e.target.result;
+      };
+      reader.readAsDataURL(browseButton.files[0]);
+    } else {
+      fileNameSpan.textContent = 'Invalid file type';
+    }
+  } else {
+    fileNameSpan.textContent = 'No files selected';
+  }
 });
