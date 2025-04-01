6b35f2a1034e175c0cdaf64a7dc42555fa21e881
Kush Bharakhada <kbharakhada1@sheffield.ac.uk>
2025-04-01 13:20:06 +0100

    feat(tests): add pdf document tests

---
+++ spec/features/document_files_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature("Managing Documents") do
+  let!(:user) { create(:user) }
+  let!(:trip) { FactoryBot.create(:trip) }
+  given!(:plan) { FactoryBot.create(:plan) }
+
+  before do
+    login_as(user, scope: :user)
+
+    # Upload a single document at the start
+    visit edit_trip_plan_path(trip, plan)
+    attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test.pdf"))
+    click_on "Save"
+    expect(page).to(have_content("Plan updated successfully."))
+  end
+
+  feature "Editing plans for documents" do
+    scenario "I can upload PDF documents" do
+      visit edit_trip_plan_path(trip, plan)
+
+      # Upload document
+      attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test2.pdf"))
+      click_on "Save"
+
+      expect(page).to(have_content("Plan updated successfully."))
+
+      # Click the dropdown to view documents
+      within("#documents-accordion-#{plan.id}-#{plan.trip_id}") do
+        find("button.accordion-button").click
+      end
+
+      # Sleep added for dropdown animation
+      sleep_for_js
+
+      # All documents uploaded should appear, existing and new
+      expect(page).to(have_link("test.pdf"))
+      expect(page).to(have_link("test2.pdf"))
+    end
+
+    scenario "I can remove PDF documents" do
+      # Ensure the view document dropdown appears initially since a document exists
+      expect(page).to(have_css("#documents-accordion-#{plan.id}-#{plan.trip_id}"))
+
+      # Delete the document
+      visit edit_trip_plan_path(trip, plan)
+      within("table") do
+        find("tr", text: "test.pdf").click_button("Remove")
+      end
+
+      visit trip_path(plan.trip_id)
+      # Document dropdown should no longer exist as no documents exists for that plan
+      expect(page).to(have_no_css("#documents-accordion-#{plan.id}-#{plan.trip_id}"))
+    end
+  end
+end
