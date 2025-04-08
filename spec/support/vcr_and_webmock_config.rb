545052ba3bf4357795c26f9b6271bfc33856ab9d
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-04-09 01:28:42 +0200

    feat(config): ignore gitlab ci for vcr

--- spec/support/vcr_and_webmock_config.rb
+++ spec/support/vcr_and_webmock_config.rb
@@ -16,9 +16,17 @@ VCR.configure do |c|
   c.hook_into(:webmock)
   c.configure_rspec_metadata!
 
+  c.ignore_request do |request|
+    # Ignore requests for gitlab CI containers
+    URI(request.uri).host&.match?(/172\.17\.0\.\d+/) \
+      || URI(request.uri).host == "chrome"
+  end
+
   c.around_http_request do |request|
     if request.uri.include?("api.pwnedpasswords.com")
       VCR.use_cassette("pwned_passwords") { request.proceed }
+    elsif request.uri.include?("ipinfo.io")
+      VCR.use_cassette("ip_info") { request.proceed }
     else
       request.proceed
     end
