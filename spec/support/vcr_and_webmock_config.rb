8ab62d199c23803db0f22ebc69778b7de486c384
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-04-09 14:16:18 +0200

    refactor(tests): removed external dependencies from sharing tests

--- spec/support/vcr_and_webmock_config.rb
+++ spec/support/vcr_and_webmock_config.rb
@@ -10,6 +10,12 @@ WebMock.disable_net_connect!(allow_localhost: true, allow: [
   "https://selenium-release.storage.googleapis.com",
 ])
 
+WebMock.after_request do |request_signature, response|
+  unless request_signature.uri.host == "127.0.0.1"
+    puts "Request #{request_signature} was made and #{response} was returned"
+  end
+end
+
 VCR.configure do |c|
   c.ignore_localhost = true
   c.cassette_library_dir = "spec/fixtures/vcr"
