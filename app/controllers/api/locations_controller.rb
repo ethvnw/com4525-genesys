90f2af267e288cce642da5eab115437fc6648117
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-04-10 19:31:16 +0200

    refactor(api): moved photon API route to config module

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -6,18 +6,11 @@ module Api
   # Allows us to easily change location search logic (adding filters etc.) without needing to change the frontend
   class LocationsController < ApplicationController
     def search
-      api_response = HTTParty.get(external_api_url, timeout: 5)&.body
+      api_response = HTTParty.get(ApiRoutes.location_search(params[:query]), timeout: 5)&.body
 
       # Set caching headers to allow the browser to cache response from photon
       response.headers["Cache-Control"] = "public, max-age=#{1.hour}"
       render(json: api_response)
-    end
-
-    private
-
-    def external_api_url
-      "https://photon.komoot.io/api?q={query}&limit=5"
-        .sub("{query}", params[:query])
     end
   end
 end
