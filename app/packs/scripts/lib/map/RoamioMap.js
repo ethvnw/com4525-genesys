bfe033fb579ae4559092ea162167aa6ddd0bcfcc
Jack Sanders <personal@jacksanders.uk>
2025-04-21 20:29:11 +0100

    fix(map): fixed map zoom level

--- app/packs/scripts/lib/map/RoamioMap.js
+++ app/packs/scripts/lib/map/RoamioMap.js
@@ -11,6 +11,7 @@ class _RoamioMap {
    */
   constructor(mapId = 'map') {
     this.mapId = mapId;
+    this.markerFG = L.featureGroup();
   }
 
   /**
@@ -19,13 +20,22 @@ class _RoamioMap {
    */
   initialise() {
     this.map = L.map(this.mapId, MAP_CONFIG);
+    this.markerFG = L.featureGroup();
+    this.markerFG.addTo(this.map);
+
     const tileLayer = L.tileLayer(TILE_LAYER_CONFIG.url, TILE_LAYER_CONFIG.options);
     this.map.addLayer(tileLayer);
   }
 
   addMarker(latLng) {
     const marker = L.marker(latLng);
-    marker.addTo(this.map);
+    this.markerFG.addLayer(marker);
+
+    // Fit map to bounds of feature group
+    this.map.fitBounds(this.markerFG.getBounds());
+
+    // Zoom out so all markers can be viewed without being right against the map border
+    this.map.setZoom(this.map.getZoom() - 2);
   }
 }
 
