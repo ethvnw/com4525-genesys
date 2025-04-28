006248a76455a89fdda1a020956b36c6be45a3e1
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-04-28 16:31:01 +0100

    fix(map-view): ordered plans by start date on map view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -121,8 +121,8 @@ class TripsController < ApplicationController
 
     @trip = Trip.find(params[:id]).decorate
     @trip_membership = TripMembership.find_by(trip_id: @trip.id, user_id: current_user.id)
-    plans = @trip.plans.order(:start_date).decorate
-    @plan_groups = plans.group_by { |plan| plan.start_date.to_date }
+    @plans = @trip.plans.order(:start_date).decorate
+    @plan_groups = @plans.group_by { |plan| plan.start_date.to_date }
 
     stream_response("trips/show")
   end
