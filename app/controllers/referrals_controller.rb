e5ad5647ba55fdd847c70c14da5840c82e95caf5
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-04-30 21:41:00 +0100

    feat(referrals): turboified referrals form

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -2,18 +2,28 @@
 
 # Handles referrals made by users
 class ReferralsController < ApplicationController
+  include Streamable
   before_action :authenticate_user!
 
   def create
     email = params[:email].to_s.downcase
+    session[:referral_email] = email
+    message = nil
 
     if User.exists?(email: email)
-      flash[:alert] = "Cannot refer a user that already exists."
+      flash[:errors] = {
+        email: ["A user with that email already exists."],
+      }
+    elsif !(email =~ URI::MailTo::EMAIL_REGEXP)
+      flash[:errors] = {
+        email: ["Email is invalid."],
+      }
     else
       ReferralMailer.with(email: email, referrer: current_user).send_referral.deliver_later
-      flash[:notice] = "Referral email sent to #{email}."
+      session.delete(:referral_email)
+      message = { type: "success", content: "Referral email sent to #{email}." }
     end
 
-    redirect_to(home_path)
+    stream_response("referrals/create", home_path, message)
   end
 end
