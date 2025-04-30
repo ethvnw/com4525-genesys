412aad8b907b7e1d8c3aadf2c8f77873e39d4831
James March <jmarch2@sheffield.ac.uk>
2025-04-30 14:38:23 +0100

    feat(qr_codes): uploading, viewing and removing QR codes (scannable tickets) (!62) Merge branch 'feat/14-qr-codes' into 'main' with the following commits: * fix(qr_codes): fixed broken test that used an eager load
    * fix(qr_codes): applying pr suggestions part 3
    
    * chore: eslint only picked up on error after push for some reason
    
    * test(qr_codes): removed flakiness from qr tests
    
    * fix(qr_codes): applying pr suggestions part 2
    
    * fix(qr_codes): applying pr suggestions part 1
    
    * test(qr_codes): update qr show test, removing test data
    
    * chore: forgot to add qr_code_view.js changes
    
    * chore: add titles to ethan's refactor
    
    * chore: eslint
    
    * feat(qr_codes): added titles to QR Codes
    
    * feat(trips-plans): add travel plan method to plan model, fix css
    
    * fix(qr_codes): code canvas now clears when reinputting codes
    
    * fix(qr_codes): fixed broken movile view for plans form
    
    * test(qr_codes): fixed broken scannable_ticket creation during test
    
    * chore: code comments for qr_code_scan.js
    
    * fix(qr_codes): made qr navigation consistent between show and form
    
    * test(qr_codes): scannable_tickets_controller.rb tests
    
    * test(qr_codes): tests for managing_plans
    
    * feat(qr_codes): show qr codes for a plan
    
    * fix(qr_codes): repeat qr codes can no longer be added
    
    * chore: forgot to haml-lint
    
    * feat(qr_codes): viewing and removing tickets from plan edit
    
    * feat(qr_codes): create scannable_tickets and link to plan on backend
    
    * feat(qr_codes): added support for multiple qr codes
    
    * feat(qr_codes): reading qr codes using jsqr on frontend

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -10,6 +10,7 @@ class PlansController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @script_packs = ["plans_create"]
     @trip = Trip.find(params[:trip_id])
     @plan = if session[:plan_data]
       Plan.new(session[:plan_data])
@@ -24,6 +25,14 @@ class PlansController < ApplicationController
     @plan = Plan.new(plan_params)
     @plan.trip = Trip.find(params[:trip_id])
     if @plan.save
+      # Create scannable tickets if provided
+      qr_codes = params[:scannable_tickets].present? ? JSON.parse(params[:scannable_tickets]) : []
+      qr_titles = params[:scannable_ticket_titles].present? ? JSON.parse(params[:scannable_ticket_titles]) : []
+
+      qr_codes.each_with_index do |code, index|
+        @plan.scannable_tickets.create(code: code, title: qr_titles[index], ticket_format: :qr)
+      end
+
       session.delete(:plan_data)
       turbo_redirect_to(trip_path(@plan.trip), notice: "Plan created successfully.")
     else
@@ -48,6 +57,7 @@ class PlansController < ApplicationController
   end
 
   def edit
+    @script_packs = ["plans_create"]
     @trip = Trip.find(params[:trip_id])
     @plan = Plan.find(params[:id]).decorate
     @errors = flash[:errors]
@@ -61,7 +71,24 @@ class PlansController < ApplicationController
       if documents
         @plan.documents.attach(documents)
       end
-      turbo_redirect_to(trip_path(@plan.trip), notice: "Plan updated successfully.")
+
+      # Create scannable tickets if provided
+      any_duplicate_codes = false
+      qr_codes = params[:scannable_tickets].present? ? JSON.parse(params[:scannable_tickets]) : []
+      qr_titles = params[:scannable_ticket_titles].present? ? JSON.parse(params[:scannable_ticket_titles]) : []
+
+      qr_codes.each_with_index do |code, index|
+        # Check if the code already exists before creating a new one
+        if @plan.scannable_tickets.exists?(code: code)
+          any_duplicate_codes = true
+        else
+          @plan.scannable_tickets.create(code: code, title: qr_titles[index], ticket_format: :qr)
+        end
+      end
+
+      # The notice message indicates whether any QR codes already existed to the plan
+      turbo_redirect_to(trip_path(@plan.trip), notice: "Plan updated successfully.
+      #{any_duplicate_codes ? "Some QR codes already existed..." : ""}")
     else
       flash[:errors] = @plan.errors.to_hash(true)
       stream_response("plans/update", edit_trip_plan_path(@plan))
@@ -72,6 +99,16 @@ class PlansController < ApplicationController
     @plan = Plan.find(params[:id])
     @plan.destroy
     turbo_redirect_to(trip_path(@plan.trip), notice: "Plan deleted successfully.")
+  end
+
+  def show
+    @script_packs = ["plans_show"]
+    @plan = Plan.find(params[:id]).decorate
+    @trip = @plan.trip.decorate
+    # Redirect back to the trip page if there are no tickets
+    if @plan.scannable_tickets.empty?
+      redirect_back_or_to(trip_path(@trip), notice: "No tickets available for this plan.")
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :trips do
     resources :plans do
+      resources :scannable_tickets, only: [:destroy]
       resources :documents, only: [:destroy]
     end
     resources :trip_memberships do
