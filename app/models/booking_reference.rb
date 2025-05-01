b1077710d9f1bdee25ead6ae540f26da0df4fe46
James <jameswmarch2002@gmail.com>
2025-05-01 20:57:19 +0100

    feat(bookings): added db migration and model update for booking refs

---
+++ app/models/booking_reference.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: booking_references
+#
+#  id                :bigint           not null, primary key
+#  booking_reference :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  plan_id           :bigint           not null
+#
+# Indexes
+#
+#  index_booking_references_on_plan_id  (plan_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (plan_id => plans.id)
+#
+class BookingReference < ApplicationRecord
+  belongs_to :plan
+end
