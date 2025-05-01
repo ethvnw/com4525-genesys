2946c4376d0c9fe0085ba531e196351f14cfdc84
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-01 23:10:08 +0100

    fix(performance): added counter caches to plans and trip memberships

--- db/migrate/20250501211420_add_plans_count_to_trips.rb
+++ db/migrate/20250501211420_add_plans_count_to_trips.rb
@@ -1,5 +1,6 @@
 class AddPlansCountToTrips < ActiveRecord::Migration[7.0]
   def change
-    add_column :trips, :plans_count, :integer, default: 0, null: false
+    add_column :trips, :regular_plans_count, :integer, default: 0, null: false
+    add_column :trips, :travel_plans_count, :integer, default: 0, null: false
   end
 end
