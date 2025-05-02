77700f37fa97fb35d77c13bbf081941041969501
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-02 12:28:16 +0100

    fix(counter-cache): fixed plans counter caches

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -34,5 +34,113 @@ require "rails_helper"
 require_relative "../concerns/countable_shared_examples"
 
 RSpec.describe(Plan, type: :model) do
+  let(:trip) { create(:trip) }
+
   it_behaves_like "countable"
+
+  describe "#add_counter_cache" do
+    context "when creating a travel plan" do
+      let(:plan) { create(:plan, trip: trip, plan_type: "travel_by_foot") }
+      it "increments travel_plans_count" do
+        expect { plan }.to(change(trip, :travel_plans_count).by(1))
+      end
+
+      it "doesn't change regular_plans_count" do
+        expect { plan }.to(change(trip, :regular_plans_count).by(0))
+      end
+    end
+
+    context "when creating a regular plan" do
+      let(:plan) { create(:plan, trip: trip, plan_type: "active") }
+      it "increments regular_plans_count" do
+        expect { plan }.to(change(trip, :regular_plans_count).by(1))
+      end
+
+      it "doesn't change travel_plans_count" do
+        expect { plan }.to(change(trip, :travel_plans_count).by(0))
+      end
+    end
+  end
+
+  describe "#remove_counter_cache" do
+    context "when removing a travel plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "travel_by_foot") }
+      subject { plan.destroy! }
+
+      it "decrements travel_plans_count" do
+        expect { subject }.to(change(trip, :travel_plans_count).by(-1))
+      end
+
+      it "doesn't change regular_plans_count" do
+        expect { plan }.to(change(trip, :regular_plans_count).by(0))
+      end
+    end
+
+    context "when removing a regular plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "active") }
+      subject { plan.destroy! }
+
+      it "decrements regular_plans_count" do
+        expect { subject }.to(change(trip, :regular_plans_count).by(-1))
+      end
+
+      it "doesn't change travel_plans_count" do
+        expect { plan }.to(change(trip, :travel_plans_count).by(0))
+      end
+    end
+  end
+
+  describe "#update_counter_cache" do
+    context "when updating a travel plan to be a regular plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "travel_by_foot") }
+      subject { plan.update!(plan_type: "active") }
+
+      it "increments the trip's regular_plans_count" do
+        expect { subject }.to(change(trip, :regular_plans_count).by(1))
+      end
+
+      it "decrements the trip's travel_plans_count" do
+        expect { subject }.to(change(trip, :travel_plans_count).by(-1))
+      end
+    end
+
+    context "when updating a travel plan to be a different travel plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "travel_by_foot") }
+      subject { plan.update!(plan_type: "travel_by_boat") }
+
+      it "doesn't change the trip's regular_plans_count" do
+        expect { subject }.to(change(trip, :regular_plans_count).by(0))
+      end
+
+      it "doesn't change the trip's travel_plans_count" do
+        expect { subject }.to(change(trip, :travel_plans_count).by(0))
+      end
+    end
+
+    context "when updating a regular plan to be a travel plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "active") }
+      subject { plan.update!(plan_type: "travel_by_foot", end_location_name: "Mock End Location Name") }
+
+      it "decrements the trip's regular_plans_count" do
+        expect { subject }.to(change(trip, :regular_plans_count).by(-1))
+      end
+
+      it "increments the trip's travel_plans_count" do
+        expect { subject }.to(change(trip, :travel_plans_count).by(1))
+      end
+    end
+
+    context "when updating a regular plan to be a different type of regular plan" do
+      let!(:plan) { create(:plan, trip: trip, plan_type: "active") }
+      subject { plan.update!(plan_type: "other") }
+
+      it "doesn't change the trip's regular_plans_count" do
+        expect { subject }.to(change(trip, :regular_plans_count).by(0))
+      end
+
+      it "doesn't change the trip's travel_plans_count" do
+        expect { subject }.to(change(trip, :travel_plans_count).by(0))
+      end
+    end
+  end
 end
