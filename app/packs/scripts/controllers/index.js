ea3bb08109b98d5db08a20d86d51ad848350029d
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-05 11:31:54 +0100

    fix(js-loading): added stimulus resolver for lazy controller loading

--- app/packs/scripts/controllers/index.js
+++ app/packs/scripts/controllers/index.js
@@ -1,17 +1,7 @@
+/* eslint-disable import/no-unresolved */
+import StimulusControllerResolver from 'stimulus-controller-resolver';
 import application from './application';
-import TripsMapController from './trips_map_controller';
-import PlanFormController from './plan_form_controller';
-import TripFormController from './trip_form_controller';
-import PlanQRController from './plan_qr_controller';
-import BookingReferencesController from './booking_references_controller';
-import TicketLinksController from './ticket_links_controller';
-import CustomImageController from './custom_image_controller';
 
-application.register('trips-map', TripsMapController);
-application.register('plans-map', TripsMapController);
-application.register('plan-form', PlanFormController);
-application.register('trip-form', TripFormController);
-application.register('plan-qr', PlanQRController);
-application.register('booking-references', BookingReferencesController);
-application.register('ticket-links', TicketLinksController);
-application.register('custom-image', CustomImageController);
+StimulusControllerResolver.install(application, async (controllerName) => (
+  (await import(`./${controllerName}-controller.js`)).default
+));
