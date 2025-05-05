a85d20950a63b15058241062240558350a503642
James <jameswmarch2002@gmail.com>
2025-05-05 11:31:06 +0100

    fix(plans): fix formatting for dates on plans

--- app/decorators/plan_decorator.rb
+++ app/decorators/plan_decorator.rb
@@ -5,12 +5,31 @@ class PlanDecorator < ApplicationDecorator
   delegate_all
   decorates_association :documents, with: DocumentDecorator
 
+  # Formats the start date to the formats:
+  # - "dd" for the same month and year
+  # - "dd mmm" for the same year
+  # - "dd mmm yyyy  for different years
+  # - end date is always formatted as "dd mmm yyyy"
   def formatted_date_range
-    if object.end_date.present?
-      "#{object.start_date.strftime("%H:%M")} to #{formatted_end_date}"
+    start_date_format = if start_date.year == end_date.year
+      if start_date.month == end_date.month
+        if start_date.day == end_date.day
+          ""
+        else
+          "%d - "
+        end
+      else
+        "%d %b - "
+      end
     else
-      object.start_date.strftime("%H:%M")
+      "%d %b %Y - "
     end
+
+    start_date.strftime(start_date_format) + end_date.strftime("%d %b %Y")
+  end
+
+  def formatted_time_range
+    start_date.strftime("%H:%M") + " - " + end_date.strftime("%H:%M")
   end
 
   def formatted_end_date
