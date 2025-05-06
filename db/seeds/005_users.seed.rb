cdd5d5d7f1712b4b51064a2587647a6ec85e0f5a
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-06 14:06:32 +0100

    fix(seeds): prevented crash if user email was changed

--- db/seeds/005_users.seed.rb
+++ db/seeds/005_users.seed.rb
@@ -4,80 +4,80 @@ require_relative "seed_helpers"
 
 # Use create() unless exists?
 # As find_or_create_by! will crash due to password field
-User.create!(
+User.create(
   email: "admin@genesys.com",
   username: "admin1",
   password: "AdminGenesys#1",
   password_confirmation: "AdminGenesys#1",
   user_role: User.user_roles[:admin],
   invitation_accepted_at: Time.zone.parse("2024-06-17 14:23:36"),
-) unless User.find_by(email: "admin@genesys.com").present?
+) unless User.find_by(email: "admin@genesys.com").or(User.find_by(username: "admin1")).present?
 
-User.create!(
+User.create(
   email: "reporter@genesys.com",
   username: "reporter1",
   password: "ReporterGenesys#1",
   password_confirmation: "ReporterGenesys#1",
   user_role: User.user_roles[:reporter],
   invitation_accepted_at: Time.zone.parse("2024-09-03 08:45:12"),
-) unless User.find_by(email: "reporter@genesys.com").present?
+) unless User.find_by(email: "reporter@genesys.com").or(User.find_by(username: "reporter1")).present?
 
-User.create!(
+User.create(
   email: "kbharakhada1@sheffield.ac.uk",
   username: "barracuda",
   password: "KBGenesys12!",
   password_confirmation: "KBGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2024-11-22 19:36:45"),
-) unless User.find_by(email: "kbharakhada1@sheffield.ac.uk").present?
+) unless User.find_by(email: "kbharakhada1@sheffield.ac.uk").or(User.find_by(username: "barracuda")).present?
 
-sanders = User.create!(
+sanders = User.create(
   email: "jsanders4@sheffield.ac.uk",
   username: "jacksanders",
   password: "JSGenesys12!",
   password_confirmation: "JSGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2024-08-30 02:17:51"),
-) unless User.find_by(email: "jsanders4@sheffield.ac.uk").present?
+) unless User.find_by(email: "jsanders4@sheffield.ac.uk").or(User.find_by(username: "jacksanders")).present?
 
 seed_avatar(sanders, "sanders.jpg") unless sanders.nil?
 
-burke = User.create!(
+burke = User.create(
   email: "jburke5@sheffield.ac.uk",
   username: "jackburke",
   password: "JBGenesys12!",
   password_confirmation: "JBGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2025-01-14 11:09:27"),
-) unless User.find_by(email: "jburke5@sheffield.ac.uk").present?
+) unless User.find_by(email: "jburke5@sheffield.ac.uk").or(User.find_by(username: "jackburke")).present?
 
 seed_avatar(burke, "burke.jpg") unless burke.nil?
 
-james = User.create!(
+james = User.create(
   email: "jmarch2@sheffield.ac.uk",
   username: "jamesmarch",
   password: "JMGenesys12!",
   password_confirmation: "JMGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2024-05-26 15:42:08"),
-) unless User.find_by(email: "jmarch2@sheffield.ac.uk").present?
+) unless User.find_by(email: "jmarch2@sheffield.ac.uk").or(User.find_by(username: "jamesmarch")).present?
 
 seed_avatar(james, "march.jpg") unless james.nil?
 
-User.create!(
+User.create(
   email: "ebarker5@sheffield.ac.uk",
   username: "ninabarker",
   password: "NBGenesys12!",
   password_confirmation: "NBGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2025-02-08 21:33:40"),
-) unless User.find_by(email: "ebarker5@sheffield.ac.uk").present?
+) unless User.find_by(email: "ebarker5@sheffield.ac.uk").or(User.find_by(username: "ninabarker")).present?
 
-User.create!(
+User.create(
   email: "eawatts1@sheffield.ac.uk",
   username: "ethanwatts",
   password: "EWGenesys12!",
   password_confirmation: "EWGenesys12!",
   user_role: User.user_roles[:member],
   invitation_accepted_at: Time.zone.parse("2024-07-19 05:58:22"),
-) unless User.find_by(email: "eawatts1@sheffield.ac.uk").present?
+) unless User.find_by(email: "eawatts1@sheffield.ac.uk").or(User.find_by(username: "ethanwatts")).present?
