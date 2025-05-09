ff66a1fa4825a6795e6d4e12fbf5a217396bf8b4
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-09 12:27:52 +0100

    fix(rake): added counter cache task as a post-migrate hook

--- db/migrate/20250509093201_add_counter_caches_to_plans.rb
+++ db/migrate/20250509093201_add_counter_caches_to_plans.rb
@@ -3,6 +3,5 @@ class AddCounterCachesToPlans < ActiveRecord::Migration[7.0]
     add_column :plans, :scannable_tickets_count, :integer, default: 0, null: false
     add_column :plans, :booking_references_count, :integer, default: 0, null: false
     add_column :plans, :ticket_links_count, :integer, default: 0, null: false
-    Rake::Task['db:set_counter_caches'].invoke
   end
 end
