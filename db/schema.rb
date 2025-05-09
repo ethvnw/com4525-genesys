4668e4f2c5b68d381ddccee904d5bafb2d0e6165
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-09 15:57:04 +0100

    feat(user): added trip counter to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_09_093201) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_09_125917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -277,6 +277,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_09_093201) do
     t.integer "invitations_count", default: 0
     t.string "username"
     t.integer "referrals_count", default: 0, null: false
+    t.integer "trips_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
