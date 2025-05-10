31a7759aceaad6ef78ed04c1f60ef8f20de97269
Jack Burke <jburke5@sheffield.ac.uk>
2025-05-10 20:25:24 +0100

    feat(error): custom error pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Make sure we know about it if params haven't been permitted
   config.action_controller.action_on_unpermitted_parameters = :raise
+
+  #config.consider_all_requests_local = false
 end
