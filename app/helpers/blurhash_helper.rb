23e3bdf332e6973fc795b210ed056d3a092a08a8
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-11 22:15:25 +0100

    feat(pagy): turboified pagination

--- app/helpers/blurhash_helper.rb
+++ app/helpers/blurhash_helper.rb
@@ -29,7 +29,10 @@ module BlurhashHelper
           image,
           options,
         ) + tag.canvas(
-          style: "position: absolute; inset: 0; transition-property: opacity; transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); transition-duration: 150ms;", class: canvas_class,
+          style: "position: absolute; inset: 0; transition-property: opacity; " \
+            "transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); " \
+            "transition-duration: 150ms;",
+          class: canvas_class,
         )
       end
     else
