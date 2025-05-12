539251dfb27f9f5bf2e448655abc838d0f6ec30c
Jack Burke <jburke5@sheffield.ac.uk>
2025-05-12 16:19:20 +0100

    fix(documents): fixed documents spec

--- spec/features/document_files_spec.rb
+++ spec/features/document_files_spec.rb
@@ -13,7 +13,13 @@ RSpec.feature("Managing Documents") do
 
     # Upload a single document at the start
     visit edit_trip_plan_path(trip, plan)
-    attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test.pdf"))
+    
+    within("#accordion-documents") do
+      find("button.accordion-button").click
+      expect(page).to have_selector("input#plan_documents", visible: true)
+      attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test.pdf"))
+    end
+    
     click_on "Save"
     expect(page).to(have_content("Plan updated successfully."))
   end
@@ -22,35 +28,47 @@ RSpec.feature("Managing Documents") do
     scenario "I can upload PDF documents" do
       visit edit_trip_plan_path(trip, plan)
 
-      # Upload document
-      attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test2.pdf"))
+      within("#accordion-documents") do
+        find("button.accordion-button").click
+        expect(page).to have_selector("input#plan_documents", visible: true)
+        attach_file("plan_documents", Rails.root.join("spec", "support", "files", "test2.pdf"))
+      end
+
       click_on "Save"
 
       expect(page).to(have_content("Plan updated successfully."))
 
-      # Click the dropdown to view documents
-      within("#documents-accordion-#{plan.id}-#{plan.trip_id}") do
-        find("button.accordion-button").click
-      end
+      Bullet.enable = false
+      visit trip_plan_path(trip, plan)
+      Bullet.enable = true
 
-      # All documents uploaded should appear, existing and new
-      expect(page).to(have_link("test.pdf"))
-      expect(page).to(have_link("test2.pdf"))
+      within("#accordion-documents") do
+        find("button.accordion-button").click
+        # All documents uploaded should appear, existing and new
+        expect(page).to(have_link("test.pdf"))
+        expect(page).to(have_link("test2.pdf"))
+      end
     end
 
-    scenario "I can remove PDF documents" do
-      # Ensure the view document dropdown appears initially since a document exists
-      expect(page).to(have_css("#documents-accordion-#{plan.id}-#{plan.trip_id}"))
-
+    scenario "I can remove PDF documents", js: true do
+      
       # Delete the document
       visit edit_trip_plan_path(trip, plan)
       # Because the document name is inside a span, it cannot be selected directly
       # However since there is only one document, the first one can be selected
-      click_on "Remove"
+      within("#accordion-documents") do
+        find("button.accordion-button").click
+        expect(page).to have_selector("input#plan_documents", visible: true)
+        accept_alert do
+          click_on "Remove"
+        end
+      end
 
-      visit trip_path(plan.trip_id)
+      Bullet.enable = false
+      visit trip_plan_path(trip, plan)
+      Bullet.enable = true
       # Document dropdown should no longer exist as no documents exists for that plan
-      expect(page).to(have_no_css("#documents-accordion-#{plan.id}-#{plan.trip_id}"))
+      expect(page).to(have_no_css("#accordion-documents"))
     end
   end
 end
