eb183017551df3a80aa27bfd8f3b94b28c6e207a
Jack Burke <jburke5@sheffield.ac.uk>
2025-05-13 01:52:28 +0100

    fix(spec): fixing issues with spec

--- app/packs/scripts/controllers/trip-form-controller.js
+++ app/packs/scripts/controllers/trip-form-controller.js
@@ -3,7 +3,6 @@ import L from 'leaflet';
 import { newRoamioMap } from '../lib/map/RoamioMap';
 import createAutocomplete from '../lib/AlgoliaAutocomplete';
 import { MAP_ICONS } from '../lib/map/map_config';
-import setupPicker from '../date_range_picker';
 import { showOverlay, hideOverlay, addSearchLocationButtonHandler } from '../lib/map/map_overlay';
 import setupPicker from '../date_range_picker_combined';
 
