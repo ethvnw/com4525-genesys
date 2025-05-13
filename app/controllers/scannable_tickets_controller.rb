61799234d62e84d3666f323e0968cb14446eeeb4
James March <jmarch2@sheffield.ac.uk>
2025-05-13 22:43:55 +0100

    fix: qr code performance, ux and services (!90) Merge branch 'fix/qr-code-fixes' into 'main' with the following commits: * chore: eslint - redundant else after return
    * fix(qr_codes): can only upload a max of 25 images at a time, save on performance
    
    * fix(qr_codes): turbo for deleting qr_codes +_fix document delete not removing table
    
    * fix(qr_codes): service class + display first new qr

--- app/controllers/scannable_tickets_controller.rb
+++ app/controllers/scannable_tickets_controller.rb
@@ -2,14 +2,20 @@
 
 # Handles the deletion of scannable tickets
 class ScannableTicketsController < ApplicationController
+  include Streamable
   before_action :authenticate_user!
   before_action :set_plan
 
   def destroy
+    @plan = Plan.find(params[:plan_id])
+    @scannable_ticket_id = params[:ticket_id]
     scannable_ticket = @plan.scannable_tickets.find(params[:ticket_id])
-    if scannable_ticket.destroy
-      redirect_back_or_to(trip_path(@plan.trip), notice: "Scannable ticket deleted successfully.")
-    end
+    scannable_ticket.destroy
+    stream_response(
+      "scannable_tickets/destroy",
+      edit_trip_plan_path(@plan.trip, @plan),
+      { type: "success", content: "Scannable Ticket deleted successfully." },
+    )
   end
 
   private
