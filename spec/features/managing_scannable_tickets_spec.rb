fe345e7998a9a3be0eee37c15397fd2321be2c95
Jack Burke <jburke5@sheffield.ac.uk>
2025-05-13 02:07:08 +0100

    fix(scannable-tickets): fixed scannable tickets not being deletable

--- spec/features/managing_scannable_tickets_spec.rb
+++ spec/features/managing_scannable_tickets_spec.rb
@@ -101,13 +101,17 @@ RSpec.feature("Managing scannable tickets") do
   feature "Delete a scannable ticket" do
     let(:plan_with_ticket) { create(:scannable_ticket, plan: create(:plan, trip: trip)).plan }
 
-    scenario "I can delete a scannable and see it removed from the plan" do
+    scenario "I can delete a scannable and see it removed from the plan", js:true do
       # Check that the ticket is present in the plan
       visit edit_trip_plan_path(trip, plan_with_ticket)
-      within("#scannable-tickets-table") do
+      within("#accordion-qr") do
+        find("button.accordion-button").click
         expect(page).to(have_content("Mock ticket code"))
       end
-      click_on("Remove")
+      accept_alert do
+        click_on("Remove")
+      end
+
       expect(page).to(have_content("Scannable ticket deleted successfully."))
       # Check that the ticket is removed from the plan
       visit edit_trip_plan_path(trip, plan_with_ticket)
