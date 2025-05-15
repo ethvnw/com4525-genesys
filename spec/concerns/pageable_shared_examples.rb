5e8a1031817c46b19ea30c85a434d8b3aae1f02a
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-15 22:31:00 +0100

    fix(tests): fixed pagination tests

--- spec/concerns/pageable_shared_examples.rb
+++ spec/concerns/pageable_shared_examples.rb
@@ -2,6 +2,11 @@
 
 require "rails_helper"
 
+##
+# Asserts that the desktop pager is present
+#
+# @param per_page [Integer] the number of items that are shown per page
+# @param total [Integer] the total number of items
 def assert_desktop_pager(per_page, total)
   pager_href = find("#pager-desktop .page-item.next a")["href"]
   query_params = CGI.parse(URI.parse(pager_href).query || "")
@@ -14,6 +19,11 @@ def assert_desktop_pager(per_page, total)
   expect(query_params).to(include("page" => ["2"]))
 end
 
+##
+# Asserts that the mobile pager is present
+#
+# @param per_page [Integer] the number of items that are shown per page
+# @param total [Integer] the total number of items
 def assert_infinite_scroll(per_page, total)
   pager_href = find("#pager-mobile a")["href"]
   query_params = CGI.parse(URI.parse(pager_href).query || "")
@@ -27,6 +37,14 @@ def assert_infinite_scroll(per_page, total)
 end
 
 RSpec.shared_examples_for("responsive pager with infinite scroll") do |per_page, total|
+  before do
+    @original_size = page.driver.browser.manage.window.size
+  end
+
+  after do
+    page.driver.browser.manage.window.resize_to(@original_size.width, @original_size.height)
+  end
+
   scenario "On desktop mode", js: true do
     page.driver.browser.manage.window.resize_to(1600, 900)
     assert_desktop_pager(per_page, total)
@@ -39,6 +57,14 @@ RSpec.shared_examples_for("responsive pager with infinite scroll") do |per_page,
 end
 
 RSpec.shared_examples_for("regular pager") do |per_page, total|
+  before do
+    @original_size = page.driver.browser.manage.window.size
+  end
+
+  after do
+    page.driver.browser.manage.window.resize_to(@original_size.width, @original_size.height)
+  end
+
   scenario "On desktop mode", js: true do
     page.driver.browser.manage.window.resize_to(1600, 900)
     assert_desktop_pager(per_page, total)
