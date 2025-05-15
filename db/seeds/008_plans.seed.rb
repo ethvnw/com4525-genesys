0fe1eee2ed93b69ca701d25109203e63a59ab131
Jack Sanders <jsanders4@sheffield.ac.uk>
2025-05-15 01:42:39 +0100

    fix(seeds): used title instead of location for plan seeds

--- db/seeds/008_plans.seed.rb
+++ db/seeds/008_plans.seed.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-brienz = Trip.find_by(location_name: "Brienz, Switzerland")
-brighton = Trip.find_by(location_name: "Brighton, United Kingdom")
-sheffield = Trip.find_by(location_name: "Sheffield, United Kingdom")
-australia = Trip.find_by(location_name: "Australia")
-europe = Trip.find_by(location_name: "Europe")
+brienz = Trip.find_by(title: "Switzerland Summer '25")
+brighton = Trip.find_by(location_name: "Beach Weekend Getaway")
+sheffield = Trip.find_by(location_name: "Graduation!")
+australia = Trip.find_by(location_name: "Australian Adventure")
+europe = Trip.find_by(location_name: "Summer Interrailing")
 
 ### BRIENZ
 Plan.find_or_create_by!(
